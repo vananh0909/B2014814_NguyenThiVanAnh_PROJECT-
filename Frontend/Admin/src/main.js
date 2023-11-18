5bd2a23d7c1fcb034bd792a446581aef53840a1b
vananh0909 <anhb2014814@student.ctu.edu.vn>
2023-11-18 20:36:32 +0700

    .

--- Frontend/Admin/src/main.js
+++ Frontend/Admin/src/main.js
@@ -1,6 +1,9 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+import router from './router/router'
+
+createApp(App).use(router).mount('#app')
--- Frontend/Admin/vite.config.js
+++ Frontend/Admin/vite.config.js
@@ -7,7 +7,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
-  ],
+  ],server: {
+    port: 3001,
+    },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
